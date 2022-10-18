a8e9390a4655ca3d74a9674f8a3b7e19c0ce3d16
Cale MacDonald <cale.macdonald@gmail.com>
2022-10-18 15:46:42 -0600

    further warning cleanup, re-enable windows TTY sizer

--- pkg/cmd/root/help.go
+++ pkg/cmd/root/help.go
@@ -170,14 +170,6 @@ func rootHelpFunc(f *cmdutil.Factory, command *cobra.Command, args []string) {
 		}
 		sort.Strings(helpTopics)
 		helpEntries = append(helpEntries, helpEntry{"HELP TOPICS", strings.Join(helpTopics, "\n")})
-
-		//if exts := f.ExtensionManager.List(); len(exts) > 0 {
-		//	var names []string
-		//	for _, ext := range exts {
-		//		names = append(names, ext.Name())
-		//	}
-		//	helpEntries = append(helpEntries, helpEntry{"EXTENSION COMMANDS", strings.Join(names, "\n")})
-		//}
 	}
 
 	flagUsages := command.LocalFlags().FlagUsages()
--- pkg/iostreams/tty_size_windows.go
+++ pkg/iostreams/tty_size_windows.go
@@ -1,10 +1,12 @@
 package iostreams
 
-//func ttySize() (int, int, error) {
-//	f, err := os.Open("CONOUT$")
-//	if err != nil {
-//		return -1, -1, err
-//	}
-//	defer f.Close()
-//	return term.GetSize(int(f.Fd()))
-//}
+import "os"
+
+func ttySize() (int, int, error) {
+	f, err := os.Open("CONOUT$")
+	if err != nil {
+		return -1, -1, err
+	}
+	defer f.Close()
+	return term.GetSize(int(f.Fd()))
+}
